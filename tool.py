1f6341ee2dd90469e169d6f654a6af86b45f1256
Phan Trọng Khang <trongkhabgphan@gmail.com>
2026-06-11 18:03:21 +0700

    Fix indentation error

--- tool.py
+++ tool.py
@@ -1,16 +1,7 @@
 import subprocess
 
-  def mo_chrome(url):
-      if not url.startswith(('http://', 'https://')):
-          url = 'https://' + url
-      subprocess.run([
-          'am', 'start',
-          '-a', 'android.intent.action.VIEW',
-          '-d', url,
-          '-n', 'com.android.chrome/com.google.android.apps.chrome.Main'
-      ])
-      print(f"Da mo Chrome: {url}")
-
-  url = input("Nhap link: ").strip()
-  mo_chrome(url)
-  
+url = input("Nhap link: ").strip()
+if not url.startswith(("http://", "https://")):
+    url = "https://" + url
+subprocess.run(["am", "start", "-a", "android.intent.action.VIEW", "-d", url, "-n", "com.android.chrome/com.google.android.apps.chrome.Main"])
+print("Da mo Chrome: " + url)
